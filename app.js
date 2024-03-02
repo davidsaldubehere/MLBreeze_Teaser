9cd89a1b1298e4d2c14b00f9f7372cce53c2115c
David Saldubehere <davesaldub@gmail.com>
2024-03-02 12:16:45 -0500

    Add responsive navigation bar

--- app.js
+++ app.js
@@ -40,3 +40,12 @@ document.addEventListener("DOMContentLoaded", function () {
     MathJax.typeset();
   }, 1000); // Adjust time between expression appearances
 });
+
+function myFunction() {
+  var x = document.getElementById("navbar");
+  if (x.className === "nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav";
+  }
+}
